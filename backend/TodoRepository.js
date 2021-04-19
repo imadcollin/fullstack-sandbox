6c95681b7393b872fcf1ea58f534f8d3ecdad1b5
imadcollin <imad.collin@gmail.com>
2021-04-20 00:00:36 +0200

    Conut remaining days

--- backend/TodoRepository.js
+++ backend/TodoRepository.js
@@ -1,4 +1,5 @@
 const Todo = require("./models/todo.model");
+var moment = require('moment'); // require
 
 class TodoRepository {
   constructor(model) {
@@ -8,7 +9,7 @@ class TodoRepository {
   create(title, todos, completed) {
     const newTodo = {
       title: title,
-      todos: todos,
+      todos: remain(todos),
       completed: isCompleted(todos),
     };
     const todo = new this.model(newTodo);
@@ -29,11 +30,12 @@ class TodoRepository {
   }
 
   updateById(id, object) {
+    console.log("update")
     const query = { _id: id };
     return this.model.findOneAndUpdate(query, {
       $set: {
         title: object.title,
-        todos: object.todos,
+        todos: remain(object.todos),
         completed: isCompleted(object.todos),
       },
     });
@@ -45,4 +47,22 @@ const isCompleted = (todos) => {
   return false;
 };
 
+const remain=(todos)=>{
+  console.log(todos)
+  for(let i = 0 ; i<todos.length; i ++){
+    todos[i].remain= calcDays( todos[i].created, todos[i].overdue); 
+  }
+  return todos
+}
+  const calcDays=(created, over)=>{
+
+     create = moment(created).format("MM/DD/YYYY")
+   
+    
+    const date1 = new Date(create);
+    const date2 = new Date(over);
+    const diffInMs = Math.abs(date2 - date1);
+    let x= diffInMs / (1000 * 60 * 60 * 24);
+    return x; 
+  }
 module.exports = new TodoRepository(Todo);
